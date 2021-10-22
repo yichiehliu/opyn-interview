4c131c57366b05bbe9f1135ce834032a158a273a
劉宜婕 <jacvd7@gmail.com>
2021-10-22 13:05:50 +0800

    feat: add Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+  padding: 16px;
+  margin: 16px 0;
+
+  border-radius: 4px;
+  box-shadow: 0px 4px 10px rgba(222, 222, 222, 0.5);
+  background-color: var(--white);
+`
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+
+  font-weight: 500;
+  font-size: 0.75rem;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+`
---
+++ src/components/Card/index.ts
@@ -0,0 +1,3 @@
+export * from './Card'
+export * from './TxActionCard'
+export * from './TxSummaryCard'
