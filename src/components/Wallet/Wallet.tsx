46a0249bb76cef6408652c056a9934c0ee2e25d0
劉宜婕 <jacvd7@gmail.com>
2021-10-23 00:15:12 +0800

    feat: add switching network functions and correspondent chainIds

--- src/components/Wallet/Wallet.tsx
+++ src/components/Wallet/Wallet.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import styled from 'styled-components'
 
 import { ConnectButton, OrderLinkButton } from '@components/Button'
@@ -8,6 +8,8 @@ import { useWeb3React } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
 import useEagerConnect from '../../hooks/useEagerConnect'
 import useInactiveListener from '../../hooks/useInactiveListener'
+import { switchToNetwork } from '@libs/switchToNetwork'
+import { SupportedChainId } from '../../constants'
 
 interface WalletProps {
   loading: boolean
@@ -16,14 +18,25 @@ interface WalletProps {
 }
 
 export const Wallet: React.FC<WalletProps> = ({ children, loading, setLoading, setOrder }) => {
-  const { connector, activate, active, deactivate, error } = useWeb3React<Web3Provider>()
+  const { chainId, library, connector, activate, active, error } = useWeb3React<Web3Provider>()
 
+  // to set activating connector
   const [activatingConnector, setActivatingConnector] = useState<any>()
   useEffect(() => {
     if (activatingConnector && activatingConnector === connector) {
       setActivatingConnector(undefined)
     }
   }, [activatingConnector, connector])
+
+  // to set switching network
+  const [switchingNetwork, setSwitchingNetwork] = useState<any>()
+  useEffect(() => {
+    if (switchingNetwork && switchingNetwork === chainId) {
+      setSwitchingNetwork(undefined)
+    }
+  }, [switchingNetwork, chainId])
+
+  const switching = switchingNetwork === SupportedChainId.MAINNET
 
   // handle logic to eagerly connect to the injected ethereum provider, if it exists and has granted access already
   const triedEager = useEagerConnect()
@@ -36,14 +49,14 @@ export const Wallet: React.FC<WalletProps> = ({ children, loading, setLoading, s
   const connected = currentConnector === connector
   const disabled = !triedEager || !!activatingConnector || connected || !!error
 
-  // go back to initial state(first step) once lose metamask connection
+  // go back to initial state(first step) once lose metamask connection or change network
   useEffect(() => {
     setOrder(prevState => {
       return { ...prevState, currentStep: 1 }
     })
-  }, [active, setOrder])
+  }, [active, chainId, setOrder])
 
-  // load at least 2000s and then set the connector
+  // since activate speed is fast so use fakeLoading to load at least 2000s and then set the connector
   const fakeLoading = async () => {
     setLoading(true)
     return new Promise<void>(resolve => {
@@ -58,9 +71,21 @@ export const Wallet: React.FC<WalletProps> = ({ children, loading, setLoading, s
     <>
       {isMetamask ? (
         active ? (
-          children
+          chainId !== SupportedChainId.MAINNET ? (
+            <SwitchNetworkButton
+              isLoading={switching}
+              onClick={async () => {
+                setSwitchingNetwork(SupportedChainId.MAINNET)
+                await switchToNetwork({ library, chainId: SupportedChainId.MAINNET })
+              }}
+            >
+              Wrong Network
+            </SwitchNetworkButton>
+          ) : (
+            children
+          )
         ) : error ? (
-          <ErrorBox>Wallet Error</ErrorBox>
+          <ErrorBox>Connection Error</ErrorBox>
         ) : (
           <ConnectButton
             disabled={disabled}
@@ -99,4 +124,11 @@ export const ErrorBox = styled.div`
 
   background-color: var(--red);
   color: var(--white);
+`
+
+export const SwitchNetworkButton = styled(ConnectButton)`
+  background-color: var(--red);
+  &:hover {
+    background-color: var(--red);
+  }
 `
--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,12 +1,46 @@
 import { InjectedConnector } from '@web3-react/injected-connector'
 
-const ChainId = {
-  MAINNET: 1,
+export enum ChainId {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GÖRLI = 5,
+  KOVAN = 42,
+
+  MATIC = 137,
+  MATIC_TESTNET = 80001,
+  FANTOM = 250,
+  FANTOM_TESTNET = 4002,
+  XDAI = 100,
+  BSC = 56,
+  BSC_TESTNET = 97,
+
+  ARBITRUM_ONE = 42161,
+  ARBITRUM_RINKEBY = 421611,
+  OPTIMISM = 10,
+  OPTIMISTIC_KOVAN = 69,
+  AVALANCHE = 43114,
 }
 
 export const injected = new InjectedConnector({
   supportedChainIds: [
     1, // mainnet
+    3, // ropsten
+    4, // rinkeby
+    5, // goreli
+    42, // kovan
+    250, // fantom
+    4002, // fantom testnet
+    137, // matic
+    80001, // matic testnet
+    100, // xdai
+    56, // binance smart chain
+    97, // binance smart chain testnet
+    43114, // avalanche
+    42161, // arbitrum
+    421611, // arbitrum testnet
+    10, // optimism
+    69, // optimism testnet
   ],
 })
 
